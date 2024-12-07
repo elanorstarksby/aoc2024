78f877b801e467dacf0514a63c644de6b8465464
Elanor Stark <elanor.stark@sainsburys.co.uk>
2024-12-07 09:49:06 +0000

    Day 6 p2

--- day06/day06.py
+++ day06/day06.py
@@ -19,6 +19,7 @@ def leaves(location, values):
         return True
     return False
 
+
 def blocked(location, values):
     if values[location[0]][location[1]] == "#":
         return True
@@ -29,11 +30,12 @@ def p1(values):
     start = find("^", values)
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     di = 0  # direction index
-    been_to = []
+    been_to = set()
     current = (start, di)
     while True:
-        print(current)
-        been_to.append(current)
+        # this is probably overcomplicated because I read the problem wrong at first and I can't be
+        # bothered to fix it now. Fortunately I read it wrong in a way that was helpful for part 2
+        been_to.add(current)
         candidate_next = (current[0][0] + directions[di][0], current[0][1] + directions[di][1])
         if leaves(candidate_next, values):
             break
@@ -45,24 +47,52 @@ def p1(values):
             next_place = candidate_next
         current = (next_place, di)
     locations_set = set([a[0] for a in been_to])
-    for l in locations_set:
-        values[l[0]] = values[l[0]][:l[1]] + "X" + values[l[0]][l[1] + 1:]
-    for v in values:
-        print(v)
-    return len(locations_set)
+    # for l in locations_set:
+    #     values[l[0]] = values[l[0]][:l[1]] + "X" + values[l[0]][l[1] + 1:]
+    # for v in values:
+    #     print(v)
+    return locations_set, len(locations_set)
 
 
-def p2(values):
-    p1_total = 0
+def does_loop(values):
+    start = find("^", values)
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    di = 0  # direction index
+    been_to = set()
+    current = (start, di)
+    while current not in been_to:
+        been_to.add(current)
+        candidate_next = (current[0][0] + directions[di][0], current[0][1] + directions[di][1])
+        if leaves(candidate_next, values):
+            return False
+        if blocked(candidate_next, values):
+            next_place = current[0]
+            di = (di + 1) % 4
+        else:
+            candidate_next = (current[0][0] + directions[di][0], current[0][1] + directions[di][1])
+            next_place = candidate_next
+        current = (next_place, di)
+    return True
 
-    return p1_total
+
+def p2(values, locations):
+    p2_total = 0
+    for r, c in locations:
+        if values[r][c] == ".":
+            new_r = values[r][:c] + "#" + values[r][c + 1:]
+            new_values = [r for r in values]
+            new_values[r] = new_r
+            if does_loop(new_values):
+                p2_total += 1
+    return p2_total
 
 
 def main():
     values = read_in()
-    print(values)
-    print(p1(values))
-    print(p2(values))
+    # print(values)
+    locations, p1_count = p1(values.copy())
+    print(p1_count)
+    print(p2(values, locations))
 
 
 if __name__ == '__main__':
